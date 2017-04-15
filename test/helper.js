80eec8a82ec533b0be01a22b3d528209caca8c05
Vitaliy Isikov <visikov@gmail.com>
2017-04-15 18:41:53 -0400

    feat(async): Make it all async (#1)

--- test/helper.js
+++ test/helper.js
@@ -1,14 +1,18 @@
-'use strict';
-
-var Payload = require('../mocks/payload-schema');
+const Payload = require('../mocks/payload-schema');
 
 module.exports = {
+  randomPayload: () => {
+    return new Payload({
+      first: "asdasdasd",
+      second: "asdasdadadasdas"
+    })
+  },
+  makeJobs: async (jobs, Job) => {
+    for (let i in jobs) {
+      jobs[i] = new Job(jobs[i])
+      await jobs[i].save()
+    }
 
-	randomPayload: function()
-	{
-		return new Payload({
-			first: "asdasdasd",
-			second: "asdasdadadasdas"
-		});
-	}
+    return jobs
+  }
 }
